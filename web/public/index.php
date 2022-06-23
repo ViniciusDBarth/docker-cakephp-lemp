2b6a21dd1b75d808b4e9dd32c7cd27f5594413c3
viniciusdbarth <viniciusdbarth@gmail.com>
2022-06-23 14:59:23 -0300

    php 8.1

--- web/public/index.php
+++ web/public/index.php
@@ -1,1 +1,30 @@
-<?php phpinfo(); ?>
+<?php
+
+$numero = 1;
+$edivisiel = false;
+
+$resto = $numero % 10;
+echo $resto.PHP_EOL;
+
+if($numero % 10 == 0){
+    $resto = $numero % 10;
+    echo $resto;
+    echo "é divisiel por 10";
+    $edivisiel = true;
+}
+
+if($numero % 5 == 0){
+    echo "é divisiel por 5";
+    $edivisiel = true;
+}
+
+if($numero % 2 == 0){
+    echo "é divisiel por 2";
+    $edivisiel = true;
+}
+
+if(!$edivisiel){
+    echo "não é divisível por 2, 5 e 10";
+}
+
+?>
